87d86d2dbef9326d9605db5de24683a656b03e64
Александр В. Гуляев <agulyaev@jet.msk.su>
2023-03-27 18:07:23 +0300

    added more debug options

--- main.py
+++ main.py
@@ -546,6 +546,8 @@ def start():
                     reply = ssh.send_command(command)
                     time.sleep(0.2)
 
+                    sendlog(cnf_save_path, reply.result)
+
                     if reply.result:
                         filtered_result = output_filter(reply.result)
 
