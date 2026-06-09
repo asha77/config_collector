d710ec2e0bdcbd80e00bd1442cce3f33d7a60541
Alexander Gulyaev <agulyaev@jet.su>
2026-06-09 19:19:05 +0300

    Changed timeouts

--- main.py
+++ main.py
@@ -647,6 +647,7 @@ def start():
     parser = create_parser()
     namespace = parser.parse_args()
     comm_file_path_specified = False
+    print("НОВАЯ ВЕРСИЯ ДЛЯ ДЕМО!!!!")
 
     if __debug__:
         # enable_basic_logging(file=True, level="debug")
